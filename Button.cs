e07af9c8eb9185825ac968595e265b01f68350af
Wojciechduda92 <wojciechduda_92@o2.pl>
2016-05-19 23:32:47 +0200

    Button
    When button is pushed blue led is on.

---
+++ Button.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.SPOT.Hardware;
+
+    public class Button
+    {
+        public static void Main()
+        {
+            OutputPort led = new OutputPort((Cpu.Pin)63, false);
+            InterruptPort button =
+                new InterruptPort((Cpu.Pin)0, false, Port.ResistorMode.PullDown, Port.InterruptMode.InterruptEdgeLevelHigh);
+            while (true)
+                if (button.Read() == true)
+                    led.Write(true);
+                else
+                    led.Write(false);
+        }
+    }
